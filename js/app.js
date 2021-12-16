121e5dbb7afc653f66a272f4e723aa3ea4f737ac
unknown <luizc04@hotmail.com>
2021-12-16 02:12:34 -0300

    Navbar JQuery

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// Identificar o clique no menu
+// Verificar o item que foi clicado e fazer referência com o alvo
+// Verificar a distância entre o alvo e o topo
+// Animar o scroll até o alvo
+
+$('navbar a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top,
+			menuHeight = $('navbar').innerHeight();
+
+	console.log(menuHeight);
+
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight -30
+	}, 500);
+});
+
